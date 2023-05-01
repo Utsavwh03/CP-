56ad4ce30705affd4b7b6d2d19d831dc5a396743
Utsavwh03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2023-05-01 20:25:52 +0530

    Added

--- modular.cpp
+++ modular.cpp
@@ -1,5 +1,7 @@
 // #include<bits/stdc++.h>
 #include <iostream>
+#include <vector>
+#include<utility>
 using namespace std;
 using lli = long long;
 const lli mod = 1e9+7;
@@ -28,11 +30,33 @@ lli modinv(lli a)
 {
    return binaryexpo(a,mod-2);
 }
+void primefactors(lli a)
+{
+    vector<pair<lli,lli> >v;
+    for (lli i=2; i<=a; i++)
+    {
+        
+        if(a%i==0)
+        {
+           lli cnt=0;
+            while(a%i ==0)
+            {
+                cnt++;
+                a=a/i;
+            }
+            v.push_back(make_pair(i,cnt));
+        }
+    }
+   for (auto i : v)
+   {
+     cout << i.first<<" ->"<<i.second<<endl;
+   }
+}
 signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cout<<modinv(18526262);
-    
+    // cout<<modinv(18526262);
+    primefactors(60);
 }
